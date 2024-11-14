bf211255ad93f34125e853944a141c238146619b
Muhammad-Hassaan-Farooq <101121986+Muhammad-Hassaan-Farooq@users.noreply.github.com>
2024-11-14 22:17:16 +0500

    added .vcf import and export

--- src/main/java/com/example/contact_management/contacts/contollers/ContactController.java
+++ src/main/java/com/example/contact_management/contacts/contollers/ContactController.java
@@ -1,7 +1,17 @@
 package com.example.contact_management.contacts.contollers;
 
 import com.example.contact_management.contacts.dto.UpdateContactDTO;
+import com.example.contact_management.contacts.models.Contact;
+import com.example.contact_management.contacts.models.EmailAddress;
+import com.example.contact_management.contacts.models.PhoneNumber;
+import ezvcard.Ezvcard;
+import ezvcard.VCard;
+import ezvcard.property.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -11,11 +21,17 @@ import com.example.contact_management.contacts.dto.ContactResponseDTO;
 import com.example.contact_management.contacts.dto.CreateContactDTO;
 import com.example.contact_management.contacts.dto.PaginatedContactListDTO;
 import com.example.contact_management.contacts.services.ContactService;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/contacts")
 public class ContactController {
 
+    private static final Logger log = LoggerFactory.getLogger(ContactController.class);
     private final ContactService contactService;
 
     public ContactController(ContactService contactService) {
@@ -73,6 +89,116 @@ public class ContactController {
             @AuthenticationPrincipal User user
     ) {
         return contactService.search(page, size, query, user);
+    }
+
+    @GetMapping("/export")
+    public ResponseEntity<byte[]> exportContacts(@AuthenticationPrincipal User user){
+
+        PaginatedContactListDTO paginatedContactListDTO = contactService.getPaginatedContacts(0,Integer.MAX_VALUE,user);
+
+        try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            for (ContactResponseDTO contact : paginatedContactListDTO.contacts()) {
+                VCard vCard = new VCard();
+                vCard.addFormattedName(new FormattedName(contact.firstName() + " " + contact.lastName()));
+                vCard.addProperty(new Title(contact.title()));
+                for (EmailAddress emailAddress : contact.emailAddresses()) {
+                    vCard.addEmail(emailAddress.getEmail());
+                }
+                for (PhoneNumber phoneNumber : contact.phoneNumbers()) {
+                    vCard.addTelephoneNumber(phoneNumber.getNumber());
+                }
+
+                Ezvcard.write(vCard).go(outputStream);
+            }
+
+            byte[] outputData = outputStream.toByteArray();
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=contacts.vcf");
+
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .contentLength(outputData.length)
+                    .contentType(MediaType.parseMediaType("text/vcard"))
+                    .body(outputData);
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/import")
+    public ResponseEntity<String> importContacts(@RequestBody MultipartFile file, @AuthenticationPrincipal User user){
+        try {
+            List<VCard> vCardsList = Ezvcard.parse(file.getInputStream()).all();
+            for (VCard vCard:vCardsList){
+
+                FormattedName name = vCard.getFormattedName();
+                String firstName = "";
+                String lastName = "";
+                String title = "";
+                if (name!= null){
+                    String[] arr = name.getValue().split("[.\\s]+");
+                    firstName = arr[0];
+                    lastName = arr[1];
+                }
+                if (!vCard.getTitles().isEmpty()){
+                    title = vCard.getTitles().getFirst().getValue();
+                }
+
+                List<Telephone> telephoneList = vCard.getTelephoneNumbers();
+                List<PhoneNumber> numbers = new ArrayList<>();
+                if (!telephoneList.isEmpty()) {
+
+                    for (Telephone telephone : telephoneList) {
+                        String type = telephone.getParameter("TYPE");
+                        String number = telephone.getText();
+
+                        PhoneNumber phoneNumber = new PhoneNumber();
+                        phoneNumber.setNumber(number);
+                        phoneNumber.setLabel(type);
+                        numbers.add(phoneNumber);
+                    }
+                }
+
+
+
+                List<Email> emailList = vCard.getEmails();
+                List<EmailAddress> emails = new ArrayList<>();
+                if (!emailList.isEmpty()) {
+                    for (Email email : emailList) {
+                        String type = email.getParameter("TYPE");
+                        String emailAddress = email.getValue();
+
+                        EmailAddress emailAddress1 = new EmailAddress();
+
+                        emailAddress1.setEmail(emailAddress);
+                        emailAddress1.setLabel(type);
+
+                        emails.add(emailAddress1);
+
+                    }
+                }
+
+                CreateContactDTO createContactDTO = new CreateContactDTO(
+                        firstName,
+                        lastName,
+                        title,
+                        emails,
+                        numbers
+                );
+
+                contactService.createContact(createContactDTO,user);
+            }
+
+            return new ResponseEntity<>("Imported successfully",HttpStatus.OK);
+
+        }
+        catch (Exception e){
+            log.error("e: ", e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 
 
