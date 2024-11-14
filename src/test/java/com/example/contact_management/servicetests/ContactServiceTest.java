197afd1324ac6f9a6ed7eff9cc904285ac6fa550
Muhammad-Hassaan-Farooq <101121986+Muhammad-Hassaan-Farooq@users.noreply.github.com>
2024-11-14 23:31:55 +0500

    added service test

--- src/test/java/com/example/contact_management/servicetests/ContactServiceTest.java
+++ src/test/java/com/example/contact_management/servicetests/ContactServiceTest.java
@@ -3,6 +3,8 @@ package com.example.contact_management.servicetests;
 import com.example.contact_management.auth.models.User;
 import com.example.contact_management.contacts.dto.ContactResponseDTO;
 import com.example.contact_management.contacts.dto.CreateContactDTO;
+import com.example.contact_management.contacts.dto.PaginatedContactListDTO;
+import com.example.contact_management.contacts.dto.UpdateContactDTO;
 import com.example.contact_management.contacts.models.Contact;
 import com.example.contact_management.contacts.repositories.ContactRepository;
 import com.example.contact_management.contacts.services.ContactService;
@@ -14,6 +16,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -100,19 +103,79 @@ public class ContactServiceTest {
     }
     @Test
     void getContactById_UnauthorizedException() {
-        // Arrange
+
         Long contactId = 1L;
+        User user = new User();
+        user.setId(2L);
+
         Contact contact = new Contact();
         contact.setId(contactId);
         contact.setUser(new User());
-        contact.setFirstName("John");
-        contact.setLastName("Doe");
+        contact.getUser().setId(1L);
 
         when(contactRepository.findById(contactId)).thenReturn(Optional.of(contact));
 
-        // Act & Assert
         assertThrows(UnauthorizedException.class, () -> {
             contactService.getContactById(contactId, user);
         });
+
+        verify(contactRepository, times(1)).findById(contactId);
     }
+
+    @Test
+    void getPaginatedContacts_Success() {
+
+        int page = 0;
+        int size = 10;
+        Pageable pageable = PageRequest.of(page, size, Sort.by("firstName").ascending().and(Sort.by("lastName").ascending()));
+        Page<Contact> contactsPage = new PageImpl<>(List.of(new Contact(), new Contact()));
+
+        when(contactRepository.findByUser(user, pageable)).thenReturn(contactsPage);
+
+        PaginatedContactListDTO response = contactService.getPaginatedContacts(page, size, user);
+
+        assertNotNull(response);
+        verify(contactRepository, times(1)).findByUser(user, pageable);
+    }
+
+    @Test
+    void updateContactById_Success() {
+
+        Long contactId = 1L;
+        UpdateContactDTO updateContactDTO = new UpdateContactDTO("Updated", "Name", "New Title", List.of(), List.of());
+        Contact existingContact = new Contact();
+        existingContact.setId(contactId);
+        existingContact.setUser(user);
+        existingContact.setFirstName("Old");
+        existingContact.setLastName("Name");
+
+        when(contactRepository.findById(contactId)).thenReturn(Optional.of(existingContact));
+        when(contactRepository.save(any(Contact.class))).thenReturn(existingContact);
+
+
+        ContactResponseDTO response = contactService.updateContactById(contactId, user, updateContactDTO);
+
+
+        assertNotNull(response);
+        assertEquals("Updated", response.firstName());
+        assertEquals("Name", response.lastName());
+        verify(contactRepository, times(1)).findById(contactId);
+        verify(contactRepository, times(1)).save(any(Contact.class));
+    }
+
+    @Test
+    void updateContactById_ResourceNotFoundException() {
+
+        Long contactId = 1L;
+        UpdateContactDTO updateContactDTO = new UpdateContactDTO("Updated", "Name", "New Title", List.of(), List.of());
+
+        when(contactRepository.findById(contactId)).thenReturn(Optional.empty());
+
+        assertThrows(ResourceNotFoundException.class, () -> {
+            contactService.updateContactById(contactId, user, updateContactDTO);
+        });
+    }
+
+
+
 }
