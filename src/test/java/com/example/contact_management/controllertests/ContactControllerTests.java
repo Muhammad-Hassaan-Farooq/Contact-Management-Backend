c8aafbc3b1fe90c88ea1075a9ccd06e0007ed2b1
Muhammad-Hassaan-Farooq <101121986+Muhammad-Hassaan-Farooq@users.noreply.github.com>
2024-11-14 22:39:28 +0500

    added controller test

--- src/test/java/com/example/contact_management/controllertests/ContactControllerTests.java
+++ src/test/java/com/example/contact_management/controllertests/ContactControllerTests.java
@@ -1,5 +1,8 @@
 package com.example.contact_management.controllertests;
 
+import com.example.contact_management.contacts.dto.ContactResponseDTO;
+import com.example.contact_management.contacts.dto.PaginatedContactListDTO;
+import com.example.contact_management.contacts.dto.UpdateContactDTO;
 import com.example.contact_management.contacts.services.ContactService;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -8,10 +11,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
@@ -84,7 +93,7 @@ class ContactControllerTests {
     @Test
     void getContactByIdAuthorized() throws Exception {
 
-        Mockito.when(contactService.getContactById(Mockito.anyLong(), Mockito.any())).thenReturn(null);
+        Mockito.when(contactService.getContactById(anyLong(), any())).thenReturn(null);
 
         mockMvc.perform(MockMvcRequestBuilders.get("/api/contacts/1")
                 .with(SecurityMockMvcRequestPostProcessors.user("test@example.com").password("password").roles("USER"))
@@ -92,4 +101,87 @@ class ContactControllerTests {
                 .andExpect(status().isOk());
     }
 
+    @Test
+    @WithMockUser(username = "test@example.com", roles = {"USER"})
+    void getContactsAuthorized() throws Exception {
+        Mockito.when(contactService.getPaginatedContacts(Mockito.anyInt(), Mockito.anyInt(), any()))
+                .thenReturn(new PaginatedContactListDTO(
+                        List.of(),
+                        0,0
+                ));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/contacts")
+                        .param("page", "0")
+                        .param("size", "10")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(username = "test@example.com", roles = {"USER"})
+    void updateContactByIdAuthorized() throws Exception {
+        Mockito.when(contactService.updateContactById(anyLong(), any(), any(UpdateContactDTO.class)))
+                .thenReturn(new ContactResponseDTO(
+                        1L,
+                        "test",
+                        "user",
+                        "software",
+                        List.of(),
+                        List.of()
+                ));
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/contacts/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"firstName\":\"John\", \"lastName\":\"Doe\"}"))
+                .andExpect(status().isOk());
+    }
+    @Test
+    @WithMockUser(username = "test@example.com", roles = {"USER"})
+    void deleteContactByIdAuthorized() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/contacts/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Contact deleted successfully"));
+    }
+
+    @Test
+    @WithMockUser(username = "test@example.com", roles = {"USER"})
+    void searchContactsAuthorized() throws Exception {
+        Mockito.when(contactService.search(Mockito.anyInt(), Mockito.anyInt(), Mockito.anyString(), Mockito.any()))
+                .thenReturn(new PaginatedContactListDTO(
+                      List.of(),
+                      0,
+                        0
+                ));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/contacts/search")
+                        .param("page", "0")
+                        .param("size", "10")
+                        .param("query", "John")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(username = "test@example.com", roles = {"USER"})
+    void exportContactsAuthorized() throws Exception {
+        Mockito.when(contactService.getPaginatedContacts(Mockito.anyInt(), Mockito.anyInt(), Mockito.any()))
+                .thenReturn(new PaginatedContactListDTO(
+                        List.of(),
+                        0,
+                        0
+                ));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/contacts/export")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+    @Test
+    @WithMockUser(username = "test@example.com", roles = {"USER"})
+    void importContactsAuthorized() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.multipart("/api/contacts/import")
+                        .file("file", "dummy vcf content".getBytes())
+                        .contentType(MediaType.MULTIPART_FORM_DATA))
+                .andExpect(status().isOk());
+    }
 }
